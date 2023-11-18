1075abb65ab4201faec42d577d5d0227c9802d2e
Kenneth Valerio <Kennethava9@gmail.com>
2023-11-18 16:22:37 -0600

    first version

---
+++ Script.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+################################################################################
+# Script: hlahd_analysis_script.sh
+# Author: Kenneth Valerio Aguilar
+# Date: 11/18/2023
+# Version: 1.0
+# Purpose: Perform HLA genotyping analysis using hlahd.sh on multiple individuals
+# Input Requirements: Requires fastq files with specific naming convention
+# Usage: ./hlahd_analysis_script.sh
+# Output: Log files, result files, and summary table in the results directory
+################################################################################
+
+# Set the project root directory
+root="/molbio/projects/hla_genotyping/hlahd"
+
+#Iterate over the files
+for i in $root/fastq/BS*1.fastq.gz; do
+    #Extract the files basenames
+    name=basename ${i}1.fastq.gz 
+    #Uncrompres the files into fastqtemp directory
+    gunzip -c name1.fastq.gz  > fastqtmp
+
+
